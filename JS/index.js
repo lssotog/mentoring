ffdc4c6f651da20a0b4a6e99a1af0443b5683346
Eduard Camilo Rincon Aranguren <eduard.rincon@globant.com>
2023-06-16 10:34:47 -0500

    Fixing arrayExcersice function

--- JS/index.js
+++ JS/index.js
@@ -41,20 +41,58 @@ const data = [
   },
 ];
 
+const data2 = [
+  {
+    name: "Luz",
+    lastName: "Doe",
+    age: 18,
+  },
+  {
+    id: "101",
+    name: "Luz",
+    lastName: "Harris",
+    age: 23,
+  },
+  {
+    name: "Luz",
+    lastName: "Castro",
+    age: 15,
+  },
+  {
+    id: "022",
+    name: "Luz",
+    lastName: "Brother",
+    age: 32,
+  },
+  {
+    id: "051",
+    name: "Luz",
+    lastName: "You",
+    age: 20,
+  },
+  {
+    id: "055",
+    name: "Luz",
+    lastName: "Stella",
+    age: 17,
+  },
+];
+
 let filterObject = data.filter((item) => item.id && item.age > 18);
 // filterObject = filterObject.filter((item) => item.age > 18);
 // console.log("--->", filterObject);
 
-let lowToHight = filterObject.sort((a, b) => a.age - b.age);
+/*let lowToHight = filterObject.sort((a, b) => a.age - b.age);
 console.log("lowToHight", lowToHight);
 
 let hightToLow = filterObject.sort((a, b) => b.age - a.age);
-console.log("----> lowToHight", lowToHight);
+console.log("----> lowToHight", lowToHight);*/
 
 const OTHER_DATA = [1, 2, 3, 4, 5];
 
 const arrayExercise = (arr) => {
-  return data.sort((a, b) => b.age - a.age);
+  return arr.sort((a, b) => b.age - a.age);
 };
 
-arrayExercise(OTHER_DATA);
+console.log(arrayExercise(data));
+
